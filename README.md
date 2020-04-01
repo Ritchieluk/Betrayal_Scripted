f608d8ad7593ee75a93f5ad92917204ab45d32c4
Luke D Ritchie <Ritchieluk@gmail.com>
2020-04-01 11:53:16 -0400

    updates

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Betrayal_Scripted
+
+
+## Target Haunts
+#### Ghost Bride (#20)
+Omen Card: Ring
+
+Location: Balcony
+#### The Abyss Gazes Back (#22)
+Omen Card: Holy Symbol
+
+Location: Gallery
+
+#### It's Alive (#31)
+Omen Card: Spear
+
+Location: Junk Room
+
+#### Death Doth Find Us All (#44)
+Omen Card: Medallion
+
+Location: Charred Room
--- api/app.py
+++ api/app.py
@@ -1,8 +1,17 @@
 from flask import Flask, request
-import json, random
+import json, random, os.path
+from os import path
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+main_floor = [][]
+
+for i in range(60):
+    for j in range(60):
+        main_floor[i][j] = 0
+
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -15,8 +24,59 @@ def dealOmen():
     cards = json.loads(req)
     print(cards)
     print(len(cards))
+    if not path.exists('omens.json'):
+        with open('omens.json', 'w') as outfile:
+            json.dump(cards, outfile)
     for obj in cards:
         print('Card: {c}, GUID: {g}'.format(c=obj['nickname'], g=obj['guid']))
     chosen_card = random.choice(cards)
+    print(chosen_card['guid'])
+    return chosen_card['guid']
+
+@app.route('/dealEvent', methods=['POST', 'PUT'])
+def dealEvent():
+    print(request.content_type)
+    req = request.get_data()
+    events = json.loads(req)
+    print(events)
+    print(len(events))
+    if not path.exists('events.json'):
+        with open('events.json', 'w') as outfile:
+            json.dump(events, outfile)
+    for obj in events:
+        print('Card: {c}, GUID: {g}'.format(c=obj['nickname'], g=obj['guid']))
+    chosen_card = random.choice(events)
+    print(chosen_card['guid'])
+    return chosen_card['guid']
+
+@app.route('/dealItem', methods=['POST', 'PUT'])
+def dealItem():
+    print(request.content_type)
+    req = request.get_data()
+    items = json.loads(req)
+    print(items)
+    print(len(items))
+    if not path.exists('items.json'):
+        with open('items.json', 'w') as outfile:
+            json.dump(items, outfile)
+    for obj in items:
+        print('Card: {c}, GUID: {g}'.format(c=obj['nickname'], g=obj['guid']))
+    chosen_card = random.choice(items)
+    print(chosen_card['guid'])
+    return chosen_card['guid']
+
+@app.route('/dealTile', methods=['POST', 'PUT'])
+def dealTile():
+    print(request.content_type)
+    req = request.get_data()
+    tiles = json.loads(req)
+    print(tiles)
+    print(len(tiles))
+    if not path.exists('tiles.json'):
+        with open('tiles.json', 'w') as outfile:
+            json.dump(tiles, outfile)
+    for obj in tiles:
+        print('Card: {c}, GUID: {g}'.format(c=obj['nickname'], g=obj['guid']))
+    chosen_card = random.choice(tiles)
     print(chosen_card['guid'])
     return chosen_card['guid']
