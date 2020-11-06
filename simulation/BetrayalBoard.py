7163896588eb723d91a3040998c2c48585bffe93
Luke D Ritchie <Ritchieluk@gmail.com>
2020-11-06 13:56:26 -0500

    Major implementations, needs debugging

--- simulation/BetrayalBoard.py
+++ simulation/BetrayalBoard.py
@@ -1,6 +1,12 @@
+import json, random, os.path
+
 class Board():
     boardState = {"upper": [], "ground": [], "basement": []}
-    playerLocations = {}
+    playerLocations = [] # Array of tuples (floor, (x,y)) indexed by player number representing their location.
+    revealedRooms = {} # Room names mapped to types
+    roomStack = {} # Room names mapped to types
+    roomLocations = {} # Map of names to coordinate tuples
+
 
     def __init__(self, numPlayers):
         for floor in boardState.keys():
@@ -9,13 +15,18 @@ class Board():
                 for j in range(20):
                     temp.append("")
                 boardState[floor].append(temp)
-        boardState["upper"][10][10] = "Upper"
-        boardState["ground"][10][10] = "Entrance"
-        boardState["ground"][10][11] = "Foyer"
-        boardState["basement"][10][10] = "Basement"
+        self.boardState["upper"][10][10] = "Upper"
+        self.roomLocations["Upper"] = ("upper", (10,10))
+        self.boardState["ground"][10][10] = "Entrance"
+        self.roomLocations["Entrance"] = ("ground", (10,10))
+        self.boardState["ground"][10][11] = "Foyer"
+        self.roomLocations["Foyer"] = ("ground", (10,11))
+        self.boardState["basement"][10][10] = "Basement"
+        self.roomLocations["Basement"] = ("basement", (10,10))
 
         for i in range(numPlayers):
-            playerLocations[0] = ("ground", (10,10))
+            self.playerLocations[0] = ("ground", (10,10))
+        self.initializeTileStack()
         
     
 
@@ -23,27 +34,89 @@ class Board():
         """
         load in all the rooms from a json file, then shuffle them
         """
+        with open('assets/tiles.json', 'r') as outfile:
+            ids = json.load(outfile)
+            print(ids)
+            for room in ids:
+                self.roomStack.push(room["name"], room["type"])
+        print(self.roomStack)
+
 
     def getActions(self, playerNum):
         """ 
         Given a player's location, return current available
         actions for them to take, always including no-op
         """
+        moves = []
+        location = playerLocations[playerNum]
+        floor = location.first()
+        coordinates = location.second()
+        if(coordinates.first()<20):
+            moves.append(0)
+        if(coordinates.first()>0):
+            moves.append(2)
+        if(coordinates.second()<20):
+            moves.append(1)
+        if(coordinates.second()>0):
+            moves.append(3)
+        return moves
+
         
-    def addTile(self, startingTile, newTile, direction):
+    def addTile(self, location):
         """
         Given a tile, add the newTile into the tree of tiles and the
         array of existing tiles based on where the player decided to put it
         """
+        # Take a random tile from the stack
+        tileName = random.choice(self.roomStack.keys()) 
+        # Move it into the revealed rooms dict
+        self.revealedRooms[tileName] = self.roomStack[tileName]
+        del roomStack[tileName]
+        # set its new location
+        self.roomLocations[tileName] = (location.second().first(), location.second().second())
+        self.boardState[location.first()][location.second().first()][location.second.second()] = tileName
+        return self.revealedRooms[tileName]
 
-    def flipTile(self):
-        return rooms.pop()
 
-    def movePlayer(self, playerNum, newTile):
+
+    def movePlayer(self, playerNum, direction):
+        # Check if move can be made
+        type = "undefined"
+        if (self.isLegalPlayerMovement(playerNum, direction)):
+            # Move the player
+            if direction == 0:
+                self.playerLocations[playerNum].second().second() += 1
+            elif direction == 1:
+                self.playerLocations[playerNum].second().first() += 1
+            elif direction == 2:
+                self.playerLocations[playerNum].second().second() -= 1
+            elif direction == 3:
+                self.playerLocations[playerNum].second().first() -= 1
+            # Check if there is already a room tile there
+            if(self.boardState[location.first()][location.second().first()][location.second().second()] == ""):
+                # Add the tile
+                location = self.playerLocations[playerNum]
+                type = self.addTile(location)
+                return type
+        else:
+            print("ERROR ---- Unexpected movement detected ---- ")
+        return type
         
 
     def isLegalPlayerMovement(self, player, direction):
-        return True
+        if direction == 0:
+            if playerLocations[playerNum].second().second() + 1 <= 20:
+                return True
+        elif direction == 1:
+            if playerLocations[playerNum].second().first() + 1 <= 20:
+                return True
+        elif direction == 2:
+            if playerLocations[playerNum].second().second() - 1 >= 0:
+                return True
+        elif direction == 3:
+            if playerLocations[playerNum].second().first() - 1 >= 0:
+                return True
+        return False
 
     def isLegalTileAssignment(self, player, direction):
         return True
--- simulation/BetrayalGame.py
+++ simulation/BetrayalGame.py
@@ -1,17 +1,58 @@
+from BetrayalBoard import Board
+from RandomAgent import RandomAgent
+
 class BetrayalGame():
     players = []
     activePlayer = 0
     turnCount = 0
     tilesRevealed = 0
     omenCount = 0
+    hauntRevealed = False
 
     def __init__(self, agents):
-        players = agents
+        self.players = agents
+        self.board = Board()
 
     def playGame(self):
+        """
+        So long as the haunt hasn't begun, give each player a turn
+            Allowing them to progress through the mansion
+            and flip tiles and draw cards
+        """
+        # Primary game loop
+        while self.hauntRevealed == False:
+            # Each player gets a turn each round
+            for player in range(len(self.players)):
+                # Each player can take multiple actions on a turn
+                moves = self.players[player].speed
+                while moves > 0:
+                    # Get the action they are going to take
+                    action = self.players[player].getNextAction(self.board)
+                    # This means move in one of the four cardinal directions
+                    if action < 4:
+                        nextTile = self.board.movePlayer(player, action)
+                        if nextTile == "undefined":
+                            moves -= 1
+                        elif nextTile == "omen":
+                            omenCount += 1
+                            self.rollHaunt()
+                            moves = 0
+                        else:
+                            moves = 0
+                    else:
+                        print("Misunderstood action")
 
-    def playerStatus(self):
 
-    def beginHaunt(self):
+
+    def rollHaunt(self):
+        """
+        Make a random roll, if less than current Omen Count, start haunt
+        """
+        hauntRoll = 0
+        for i in range(6):
+            hauntRoll += random.choice([0,1,2])
+        if hauntRoll < self.omenCount:
+            self.hauntRevealed = True
+        return self.hauntRevealed
 
 
